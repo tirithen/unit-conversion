60451b5e4bfd162e8943f19d6ef1d56970763495
Fredrik Söderström <fredrik.soderstrom@mojlighetsministeriet.se>
2017-10-18 20:43:55 +0200

    Empty main.go file for now

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main // import "github.com/tirithen/unit-conversion"
 
-import (
-	"fmt"
-	"io/ioutil"
-)
-
 func main() {
-	configuration, err := ioutil.ReadFile("./converter.yml")
-	converter := unitconversion.NewConverterFromYAML(configuration)
 
-	input := unitconversion.Quantity{Magnitude: 10, Unit: "in"}
-	output, err := converter.Convert(input, "cm")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Print(output)
 }
